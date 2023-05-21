14467bfab0e5094cab56c7e069ce6938bdca2b24
Amodu <amodudavid777@gmail.com>
2023-05-21 22:00:52 +0100

    Records Highest Level

--- scoreboard.py
+++ scoreboard.py
@@ -1,10 +1,12 @@
 from turtle import Turtle
-FONT = ("Courier", 24, "normal")
+FONT = ("Courier", 15, "normal")
 
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        with open("data.txt", mode='r') as data:
+            self.highest_level = int(data.read())
         self.color('black')
         self.hideturtle()
         self.penup()
@@ -16,6 +18,8 @@ class Scoreboard(Turtle):
         self.clear()
         self.goto(-300, 260)
         self.write(f'LEVEL: {self.counter}', align='left', font=FONT)
+        self.goto(280, 260)
+        self.write(f'HIGHEST LEVEL: {self.highest_level}', align='right', font=FONT)
 
     def add_level(self):
         """Adds to the level"""
@@ -26,3 +30,7 @@ class Scoreboard(Turtle):
         """Game Over"""
         self.goto(0, 0)
         self.write('GAME OVER', align='center', font=FONT)
+        if self.counter > self.highest_level:
+            with open('data.txt', mode='w') as data:
+                self.highest_level = self.counter
+                data.write(f'{self.counter}')
